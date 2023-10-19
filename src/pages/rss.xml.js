d529e58ee032fff796c98ec511e777f4484ba5ce
Manuel Carbajal <manicar2093@gmail.com>
2023-10-19 01:11:46 -0600

    feat: add base url

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -12,7 +12,7 @@ export async function get(context) {
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: post.data.description,
-      link: `/blog/${post.slug}/`,
+      link: `${import.meta.env.BASE_URL}/blog/${post.slug}/`,
     })),
   });
 }
